18c0d91425359de9c1d5b72f9bc8471b46a8117a
yxb_centos <yxb@centos.virtual>
2013-03-28 23:38:03 +0800

    add cleanup plugin.

---
+++ src/cleanup_plugin_src/fsplugin.c
@@ -0,0 +1,38 @@
+#include "fsplugin.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define DATA_LIMIT_LEN 1048576 //1024*1024 Bytes, limit to 1MB data.
+int on_new_client(struct fs_context* context, int cid) {
+	return 0;
+}
+
+int on_recv_data(struct fs_context* context, int cid) {
+	struct fs_client* client;
+	client = &context->client_list->clients[cid];
+	if(client->readbuff.datalen > DATA_LIMIT_LEN) {
+		context->log(LOG_W, "cleanup: cid:%d fid:%d datalen:%d addr:%s:%d", client->cid, client->fd,
+				client->readbuff.datalen, (const char*)inet_ntoa(client->addr.sin_addr),
+				ntohs(client->addr.sin_port));
+		context->b_free(&client->readbuff);
+		return 1;
+	}
+	return 0;
+}
+
+int on_close_client(struct fs_context* context, int cid) {
+	return 0;
+}
+
+int on_write_data(struct fs_context* context, int cid) {
+	return 0;
+}
+
+int on_recv_from(struct fs_context* context, char* data, int length,
+		struct sockaddr_in* fromaddr) {
+	return 0;
+}
---
+++ src/cleanup_plugin_src/fsplugin.h
@@ -0,0 +1,14 @@
+#ifndef _FS_PLUGIN_H__
+#define _FS_PLUGIN_H__
+
+#include "fstype.h"
+
+int on_new_client(struct fs_context* context, int cid);
+int on_recv_data(struct fs_context* context, int cid);
+int on_close_client(struct fs_context* context, int cid);
+int on_write_data(struct fs_context* context, int cid);
+int on_recv_from(struct fs_context* context, char* data, int length,
+		struct sockaddr_in* fromaddr);
+
+#endif
+
